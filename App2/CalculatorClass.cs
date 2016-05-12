323e2af41b179302f8efd7e72e8956389d462a48
JohnNoxCarter <ionut.costescu@gmail.com>
2016-05-12 09:58:21 +0300

    calcualtorclass, some more globals

---
+++ App2/CalculatorClass.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    public class TruePriorityComparer : IComparer<TaskObject>
+    {
+        public int Compare(TaskObject x, TaskObject y)
+        {
+            return x.TruePriority.CompareTo(y.TruePriority);
+        }
+    }
+
+    class CalculatorClass
+    {
+        TruePriorityComparer TPC = new TruePriorityComparer();
+
+        public void ScheduleCalculate()
+        {
+            Array.Sort(Globals.TaskList, TPC);
+            DateTime StartMoment = DateTime.Now;
+            DateTime WeekDayStart = DateTime.Now, WeekDayEnd = DateTime.Now;
+            int i;
+            bool checker;
+
+            for (i=0; i<Globals.TaskList.Length; i++)
+            {
+                switch (StartMoment.DayOfWeek)
+                {
+                    case DayOfWeek.Monday:
+                        WeekDayStart = Globals.MondayStartTime;
+                        WeekDayEnd = Globals.MondayEndTime;
+                        break;
+                    case DayOfWeek.Tuesday:
+                        WeekDayStart = Globals.TuesdayStartTime;
+                        WeekDayEnd = Globals.TuesdayEndTime;
+                        break;
+                    case DayOfWeek.Wednesday:
+                        WeekDayStart = Globals.WednesdayStartTime;
+                        WeekDayEnd = Globals.WednesdayEndTime;
+                        break;
+                    case DayOfWeek.Thursday:
+                        WeekDayStart = Globals.ThursdayStartTime;
+                        WeekDayEnd = Globals.ThursdayEndTime;
+                        break;
+                    case DayOfWeek.Friday:
+                        WeekDayStart = Globals.FridayStartTime;
+                        WeekDayEnd = Globals.FridayEndTime;
+                        break;
+                    case DayOfWeek.Saturday:
+                        WeekDayStart = Globals.SaturdayStartTime;
+                        WeekDayEnd = Globals.SaturdayEndTime;
+                        break;
+                    case DayOfWeek.Sunday:
+                        WeekDayStart = Globals.SundayStartTime;
+                        WeekDayEnd = Globals.SundayEndTime;
+                        break;
+                }
+
+                checker = true;   
+
+                checker = StartMoment.AddHours(Globals.TaskList[i].EstimatedTime).TimeOfDay > WeekDayStart.TimeOfDay;
+
+                if (checker)
+                {
+                    StartMoment.AddHours(-StartMoment.Hour + WeekDayEnd.Hour);
+                }
+
+                checker = StartMoment.AddHours(Globals.TaskList[i].EstimatedTime).TimeOfDay < Globals.BedTime.TimeOfDay;
+
+                if (checker)
+                {
+                    Globals.TaskList[i].StartDate = StartMoment;
+                    Globals.TaskList[i].EndDate = Globals.TaskList[i].StartDate.AddHours(Globals.TaskList[i].EstimatedTime);
+
+                    StartMoment = Globals.TaskList[i].EndDate.AddHours(1);
+                }
+                else
+                {
+                    StartMoment.AddHours(Globals.WakeUpTime.Hour-StartMoment.Hour);
+                    StartMoment.AddMinutes(-StartMoment.Minute);
+                    i--;
+                }
+            }
+        }
+    }
+}
--- App2/Globals.cs
+++ App2/Globals.cs
@@ -9,6 +9,23 @@ namespace App2
     public static class Globals
     {
         public static TaskObject[] TaskList;
+
+        public static DateTime WakeUpTime = new DateTime(1, 1, 1, 8, 0, 0);
+        public static DateTime BedTime = new DateTime(1, 1, 1, 22, 0, 0);
+        public static DateTime MondayStartTime = new DateTime(1, 1, 1, 9, 0, 0);
+        public static DateTime MondayEndTime = new DateTime(1, 1, 1, 17, 0, 0);
+        public static DateTime TuesdayStartTime = new DateTime(1, 1, 1, 9, 0, 0);
+        public static DateTime TuesdayEndTime = new DateTime(1, 1, 1, 17, 0, 0);
+        public static DateTime WednesdayStartTime = new DateTime(1, 1, 1, 9, 0, 0);
+        public static DateTime WednesdayEndTime = new DateTime(1, 1, 1, 17, 0, 0);
+        public static DateTime ThursdayStartTime = new DateTime(1, 1, 1, 9, 0, 0);
+        public static DateTime ThursdayEndTime = new DateTime(1, 1, 1, 17, 0, 0);
+        public static DateTime FridayStartTime = new DateTime(1, 1, 1, 9, 0, 0);
+        public static DateTime FridayEndTime = new DateTime(1, 1, 1, 17, 0, 0);
+        public static DateTime SaturdayStartTime = new DateTime(1, 1, 1, 9, 0, 0);
+        public static DateTime SaturdayEndTime = new DateTime(1, 1, 1, 17, 0, 0);
+        public static DateTime SundayStartTime = new DateTime(1, 1, 1, 9, 0, 0);
+        public static DateTime SundayEndTime = new DateTime(1, 1, 1, 17, 0, 0);
     }
 }
 
